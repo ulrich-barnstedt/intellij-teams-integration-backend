7a28b62ecc23de12ea8210107dbd5e82976b8c4a
Ulrich Barnstedt <ulrich.barnstedt@gmail.com>
2022-03-10 23:51:14 +0100

    Added further testing configuration

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log(process.argv);
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    //await page.screenshot({ path: 'example.png' });
+
+    setTimeout(async () => await browser.close(), 10000);
+})();
